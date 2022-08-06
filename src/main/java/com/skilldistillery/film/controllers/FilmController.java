f01791d06b7e3ff239c301cdb30650e04d32d675
Ryan <snapsglasen@gmail.com>
2022-08-07 00:33:49 +0900

    starting controller

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,6 +1,20 @@
 package com.skilldistillery.film.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class FilmController {
+	
+	@RequestMapping("findFilmByID.do")
+	public ModelAndView processData(Film film, int filmId) {
+		String allCaps = filmId.toUpperCase();
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("view.jsp");
+		mv.addObject("result", allCaps);
+		return mv;
 
  
  
